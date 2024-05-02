4f84b967d18dbd6335ede4e33c4165f1f4337110
Rômulo Augusto <romuloffall@gmail.com>
2024-05-01 21:04:24 -0300

    #create customer validations on model

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :customer do
-    
+    name {Faker::Name.name}
+    phone {Faker::PhoneNumber.cell_phone}
+    email {Faker::Internet.email}
+    address {Faker::Address.street_address}
+    address_number {Faker::Address.building_number}
+    address_complement {Faker::Address.secondary_address}
+    neighborhood {Faker::Address.community}
+    city {Faker::Address.city}
+    state {Faker::Address.state_abbr}
+    zip_code {Faker::Address.zip_code}
+    cpf_cnpj {Faker::Company.brazilian_company_number}
   end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Customer, type: :model, focus: true do
+  it "is valid with name and phone" do
+    company = create(:company)
+    customer = create(:customer, company_id: company.id)
+    expect(customer).to be_valid
+  end
+
+  it "is invalid without name" do
+    company = create(:company)
+    customer = build(:customer, name: nil, company_id: company.id)
+    customer.valid?
+    expect(customer.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without phone" do
+    company = create(:company)
+    customer = build(:customer, phone: nil, company_id: company.id)
+    customer.valid?
+    expect(customer.errors[:phone]).to include("can't be blank")
+  end
 end
