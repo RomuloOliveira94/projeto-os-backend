d78f429086d0a51bd9af596687b644ffe8cbff18
Rômulo Augusto <romuloffall@gmail.com>
2024-05-01 23:20:35 -0300

    #implements customers controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,2 +1,51 @@
 class Api::V1::CustomersController < ApplicationController
+  include Authentication
+
+  def index
+    @customers = Customer.all
+    render status: :ok, json: @customers
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+
+    if @customer.save
+      render status: :ok, json: @customer
+    else
+      render status: :unprocessable_entity, json: { errors: @customer.errors }
+    end
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+
+    if @customer.nil?
+      render status: :not_found, json: { error: "Customer not found" }
+      return
+    end
+
+    if @customer.update(customer_params)
+      render status: :ok, json: @customer
+    else
+      render status: :unprocessable_entity, json: { errors: @customer.errors }
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+
+    if @customer.nil?
+      render status: :not_found, json: { error: "Customer not found" }
+      return
+    end
+
+    @customer.destroy
+    render status: :ok, json: { message: "Customer deleted" }
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :phone, :company_id, :email, :address, :address_number, :address_complement, :neighborhood, :city, :state, :zip_code, :cpf_cnpj)
+  end
 end
--- spec/requests/api/v1/customers_spec.rb
+++ spec/requests/api/v1/customers_spec.rb
@@ -1,7 +1,82 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Customers", type: :request do
+RSpec.describe "Api::V1::Customers", type: :request, focus: true do
+  before(:each) do
+    @user = create(:user)
+    @company = create(:company, user: @user)
+    @customer =  build(:customer, company_id: @company.id)
+    @token = JWT.encode({ user_id: @user.id }, Rails.application.credentials.secret_key_base)
+    @headers = { 'Authorization' => "Bearer #{@token}" }
+  end
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+   it "returns http success" do
+      customers = Customer.all
+      get "/api/v1/customers", headers: @headers
+      expect(response.body).to eq(customers.to_json)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST /create" do
+    it "returns http success" do
+      post "/api/v1/customers", params: { customer: @customer.attributes }, headers: @headers
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns http unprocessable_entity" do
+      post "/api/v1/customers", params: { customer: { name: nil } }, headers: @headers
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "returns http unauthorized" do
+      post "/api/v1/customers", params: { customer: @customer.attributes }
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
+  describe "PUT /update" do
+    it "returns http success" do
+      @customer.save
+      @customer.name = "Updated Name"
+      put "/api/v1/customers/#{@customer.id}", params: { customer: @customer.attributes }, headers: @headers
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns http not_found" do
+      put "/api/v1/customers/0", params: { customer: @customer.attributes }, headers: @headers
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "returns http unprocessable_entity" do
+      @customer.save
+      put "/api/v1/customers/#{@customer.id}", params: { customer: { name: nil } }, headers: @headers
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "returns http unauthorized" do
+      @customer.save
+      put "/api/v1/customers/0", params: { customer: @customer.attributes }
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it "returns http success" do
+      @customer.save
+      delete "/api/v1/customers/#{@customer.id}", headers: @headers
+      expect(response).to have_http_status(:success)
+    end
+
+    it "returns http not_found" do
+      delete "/api/v1/customers/0", headers: @headers
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "returns http unauthorized" do
+      @customer.save
+      delete "/api/v1/customers/#{@customer.id}"
+      expect(response).to have_http_status(:unauthorized)
+    end
   end
 end
